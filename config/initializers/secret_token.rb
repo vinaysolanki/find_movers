51fdd2db8169e527dc7382bb508e7d353b10e2a9
Vinay Solanki <vinaysolanki2005@gmail.com>
2014-05-13 22:15:06 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindMovers::Application.config.secret_token = '81cc94f52901bfa045780d3ffd82d1daf60b59af3f736e8b710a949825ad18ff475b90f70211c677b4d5aa6ed2a0683c1f92f1447c62cbb5f141201ca0877c2c'
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first_name: 'John', last_name: 'Doe', email: 'johndoe@gmail.com', phone_no: 7789917641)
